702d263119bf17953b7c2669e4658b7fa926aea7
Mark Myers <markalope@gmail.com>
2018-06-14 15:46:10 -0700

    adding the server app files

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 const pg = require('pg');
 const client = require('./db-client');
